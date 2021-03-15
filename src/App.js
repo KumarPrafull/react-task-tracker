df89da2e6727621b06307d392213f89bf27f43be
Kumar Prafull <eprafull@gmail.com>
2021-03-15 13:16:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 function App() {
+
+  const name = "Kumar";
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>
-       Hello React Task Tracker
+       Hello React Task
       </h1>
+      <h2>
+        This is your to-do list {name}
+      </h2>
     </div>
   );
 }
